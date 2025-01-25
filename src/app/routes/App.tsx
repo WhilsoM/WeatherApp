a931d12a3e3c0c4ded917adf019489ee47ed9848
Arthur Akhmetov <artur.akhmetov.2008@gmail.com>
2025-01-25 21:48:59 +0300

    add architecture, header, footer widgets and home page

--- src/app/routes/App.tsx
+++ src/app/routes/App.tsx
@@ -1,5 +1,17 @@
+import { Home } from '@/pages/home/Home'
+import { Route, Routes } from 'react-router'
+import { Layout } from './Layout'
+
 const App = () => {
-	return <div></div>
+	return (
+		<>
+			<Routes>
+				<Route path='/' element={<Layout />}>
+					<Route index element={<Home />} />
+				</Route>
+			</Routes>
+		</>
+	)
 }
 
 export default App
