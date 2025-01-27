63172a0b3977a06b555c7d81d89ef22d13f5d258
Arthur Akhmetov <artur.akhmetov.2008@gmail.com>
2025-01-28 01:35:02 +0300

    fix query params in the url

--- src/features/getWeather.ts
+++ src/features/getWeather.ts
@@ -1,5 +1,5 @@
-export const getWeather = async () => {
-	const response = await fetch(import.meta.env.VITE_API_URL)
+export const getWeather = async (city = 'москва') => {
+	const response = await fetch(`${import.meta.env.VITE_API_URL}&q=${city}`)
 	const data = await response.json()
 	return data
 }
