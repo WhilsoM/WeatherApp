837b5ad686597f6eea723d4371888f44cd7dd259
ZenBogdan2604 <93253276+ZenBogdan2604@users.noreply.github.com>
2025-01-28 00:22:16 +0300

    refactoring

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,11 +2,11 @@ import { Search } from '@/widgets/search/Search'
 import { SideBar } from '@/widgets/sidebar/SideBar'
 import s from './ui/home.module.scss'
 
+
 export const Home = () => {
 	return (
 		<section className='container'>
-			<Search />
-
+			<Search />			
 			<section className={s.home_page}>
 				<SideBar />
 					<p></p>
--- src/pages/memes/Memes.tsx
+++ src/pages/memes/Memes.tsx
@@ -1,11 +1,30 @@
 import s from './ui/memes.module.scss'
 
+
+
 export const Memes = () => {
   return (
       <section className={`${s.about_us} container`}>
     <div className={s.main_info}>
-        <h2 className={s.main_title}>Ты опоздал Артур, я проебал твои мемы в покер!</h2>
-        
+    <h2 className={s.main_title}>Ты опоздал Артур, я проебал твои мемы в покер!</h2>
+      <div className={s.block_main}>
+        <div className={s.block_first}>
+          <div className={s.block_create}>
+
+          </div>
+          <div className={s.block_ad}>
+
+          </div>
+        </div>
+        <div className={s.block_second}>
+          <div>
+
+          </div>
+        </div>
+      </div>
+      <div>
+
+      </div>
     </div>
  </section>
   )
