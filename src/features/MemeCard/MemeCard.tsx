c59ddf3b6fe60bd4c173845d915919f48c01c065
ZenBogdan2604 <93253276+ZenBogdan2604@users.noreply.github.com>
2025-01-28 14:23:31 +0300

    add: styles for memes

--- src/features/MemeCard/MemeCard.tsx
+++ src/features/MemeCard/MemeCard.tsx
@@ -11,11 +11,13 @@ export interface MemeCardProps {
 const MemeCard: React.FC<MemeCardProps> = ({ imageUrl, userName, createdAt, title }) => {
   return (
   <section className={s.card}>  
-      <div className={s.card_wrap}>
-    <img className={s.card_img} src={imageUrl} alt="Meme" style={{ width: '20%', height: 'auto' }} />
-    <h3 className={s.card_txt}>{title}</h3>
-    <h3 className={s.card_txt}>{userName}</h3>
-    <p>Created at: {new Date(createdAt).toLocaleDateString()}</p>
+    <div className={s.card_wrap}>
+      <img className={s.card_img} src={imageUrl} alt="Meme"/>
+      <div className={s.card_block}>
+        <h3 className={s.card_txt}>{title}</h3>
+        <h3 className={s.card_txt}>@{userName}</h3>
+        <p>Created at: {new Date(createdAt).toLocaleDateString()}</p>
+      </div>
   </div>
 </section>
   );
