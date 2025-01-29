adbc84664a4ee006b8a20c4d2cb0ff02243653b4
ZenBogdan2604 <93253276+ZenBogdan2604@users.noreply.github.com>
2025-01-29 14:44:21 +0300

    add: changeable ads

--- src/features/Ad/Ad.tsx
+++ src/features/Ad/Ad.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import s from './ui/ad.module.scss'
+import React, { useEffect, useState } from 'react';
+import s from './ui/ad.module.scss';
 import axios from 'axios';
 import { Link } from 'react-router';
 
-const API_URL = 'https://67968bd6bedc5d43a6c58fc6.mockapi.io/ad'
+const API_URL = 'https://67968bd6bedc5d43a6c58fc6.mockapi.io/ad';
 
 interface AdData {
     id: string,
@@ -15,32 +15,41 @@ interface AdData {
 
 const Ad = () => {
     const [ads, setAds] = useState<AdData[]>([]);
-    
+    const [currentAdIndex, setCurrentAdIndex] = useState(0);
+
     useEffect(() => {
         const fetchAds = async () => {
             try {
                 const response = await axios.get(API_URL);
                 setAds(response.data);
-            }catch(error){
-                console.error('Ты опоздал админ, я проебал рекламу в покер!')
+            } catch (error) {
+                console.error('Ты опоздал админ, я проебал рекламу в покер!');
             }
         };
+
         fetchAds();
-    }, []);
-  return (
-    <div className={s.adContainer}>
-    {ads.map((ad) => (
-      <div key={ad.id} className={s.ad_card}>
-        <img src={ad.img} className={s.ad_img}/>
-        <div >
-            <h2 className={s.adTitle}>{ad.title}</h2>
-            <p className={s.adText}>{ad.text}</p>
-            <Link to={ad.link}>Подробнее</Link>
+
+        const interval = setInterval(() => {
+            setCurrentAdIndex(prevIndex => (prevIndex + 1) % ads.length);
+        },30000);
+
+        return () => clearInterval(interval);
+    }, [ads.length]);
+
+    const currentAd = ads[currentAdIndex];
+
+    return (
+        <div className={s.adContainer}>
+            <div key={currentAd.id} className={s.ad_card}>
+                <img src={currentAd.img} className={s.ad_img} alt={currentAd.title} />
+                <div>
+                    <h2 className={s.adTitle}>{currentAd.title}</h2>
+                    <p className={s.adText}>{currentAd.text}</p>
+                    <Link to={currentAd.link}>Подробнее</Link>
+                </div>
+            </div>
         </div>
-      </div>
-    ))}
-  </div>
-  )
+    );
 }
 
-export default Ad
+export default Ad;
