eb03b195a999864b9e9d3085e0731812c61de73f
Arthur Akhmetov <artur.akhmetov.2008@gmail.com>
2025-02-03 01:33:37 +0300

    add new custom hook

--- src/pages/memes/Memes.tsx
+++ src/pages/memes/Memes.tsx
@@ -1,28 +1,25 @@
-import s from './ui/memes.module.scss'
-import AddMemeCard from '@/features/addmemecard/AddMemeCard'
-import Ad from '@/features/Ad/Ad'
-
-
+import Ad from "@/features/Ad/Ad";
+import { AddMemeCard } from "@/features/addmemecard/AddMemeCard";
+import s from "./ui/memes.module.scss";
 
 export const Memes = () => {
   return (
-      <section className={`${s.about_us} container`}>
-    <div className={s.main_info}>
-      <div className={s.block_main}>
-
-        <div className={s.block_second}>
-          <div>
-            <p className={s.block}><Ad /></p>
-          </div>
-          <div>
-            <AddMemeCard />
+    <section className={`${s.about_us} container`}>
+      <div className={s.main_info}>
+        <div className={s.block_main}>
+          <div className={s.block_second}>
+            <div>
+              <p className={s.block}>
+                <Ad />
+              </p>
+            </div>
+            <div>
+              <AddMemeCard />
+            </div>
           </div>
         </div>
+        <div></div>
       </div>
-      <div>
-
-      </div>
-    </div>
- </section>
-  )
-}
+    </section>
+  );
+};
