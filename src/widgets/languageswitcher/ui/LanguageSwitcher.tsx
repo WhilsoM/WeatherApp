e510a80e02d1317908b365c9aaddb7afbdce5674
Arthur Akhmetov <artur.akhmetov.2008@gmail.com>
2025-02-14 15:51:06 +0300

    add styles and using with keyboard easy elements

--- src/widgets/languageswitcher/ui/LanguageSwitcher.tsx
+++ src/widgets/languageswitcher/ui/LanguageSwitcher.tsx
@@ -11,24 +11,24 @@ export const LanguageSwitcher = () => {
   return (
     <select
       onChange={(e) => changeLanguage(e.target.value)}
-      className={s.switcher_block}
+      className={s.switcher}
     >
-      <option disabled className={s.switcher}>
+      <option disabled className={s.switcher__option}>
         Выбор языка
       </option>
-      <option value={"ru"} className={s.switcher}>
+      <option value={"ru"} className={s.switcher__option}>
         русский
       </option>
-      <option value={"en"} className={s.switcher}>
+      <option value={"en"} className={s.switcher__option}>
         английский
       </option>
-      <option value={"ja"} className={s.switcher}>
+      <option value={"ja"} className={s.switcher__option}>
         японский
       </option>
-      <option value={"kz"} className={s.switcher}>
+      <option value={"kz"} className={s.switcher__option}>
         казахский
       </option>
-      <option value={"sp"} className={s.switcher}>
+      <option value={"sp"} className={s.switcher__option}>
         испанский
       </option>
     </select>
